a4afc870e2f5e2764b9c735dc6bc5d08ae58ac65
FaiziBytes <chfaizanaslam10@gmail.com>
2025-11-27 17:11:38 +0500

    adding user validation and sanitize the data for the sql attacks

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,10 +1,18 @@
 import tryCatch from "../middlewares/trycatch.js";
-
-export const registerUser = tryCatch(async (req,res)=>{
-      const {name,email,password} = req.body;
+import sanitize from "mongo-sanitize";
+import { registerSchema } from "../validations/registerSchema.js";
+export const registerUser = tryCatch(async (req, res) => {
+      const sanitizedBody = sanitize(req.body);
+      const validation = registerSchema.safeParse(sanitizedBody);
+      if (!validation.success) {
+            return res.status(400).json({
+                  message: validation.error.issues[0].message
+            });
+      }
+      const { name, email, password } = validation.data;
       res.json({
-            name:name,
-            email:email,
-            password:password
+            name: name,
+            email: email,
+            password: password
       })
 })
---
+++ backend/validations/registerSchema.js
@@ -0,0 +1,7 @@
+import {z} from "zod";
+
+export const registerSchema = z.object({
+      name:z.string().min(3,"name must be atleast 3 characters long"),
+      email:z.string().email("invalid email format"),
+      password:z.string().min(7,"password must be atleast 8 characters long")
+})
