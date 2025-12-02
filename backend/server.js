712bdb0862b436d9de1739249cc737b09bad8bdb
FaiziBytes <chfaizanaslam10@gmail.com>
2025-12-02 14:55:57 +0500

    adding redis url

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/connection.js";
 import Router from "./routes/user.routes.js";
+import {createClient} from "redis";
 dotenv.config();
 await connectDB();
+
+const redisUrl = process.env.REDIS_URL;
+if(!redisUrl){
+      console.log("missing redis url");
+      process.exit(1);
+}
+export const redisClient = createClient({
+      url:redisUrl
+})
+redisClient.connect()
+.then(()=> console.log("connected to redis"))
+.catch(console.error)
 // our app 
 const app = express();
 // middlewares
